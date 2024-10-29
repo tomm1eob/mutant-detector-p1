7724aa2c1b0fae85fab14ece9f28f387f4857087
tomm1eob <tomiobredorsteam@gmail.com>
2024-10-29 15:34:19 -0300

    Se agrego README y el PDF (documentacion)

--- README.md
+++ README.md
@@ -1,25 +1,85 @@
-# Getting Started
+## Datos:
 
-### Reference Documentation
-For further reference, please consider the following sections:
+- **Nombre y Apellido**: Tomás Obredor
+- **Comisión**: 3k10
+- **Legajo**: 50113
 
-* [Official Gradle documentation](https://docs.gradle.org)
-* [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/3.3.4/gradle-plugin)
-* [Create an OCI image](https://docs.spring.io/spring-boot/3.3.4/gradle-plugin/packaging-oci-image.html)
-* [Spring Web](https://docs.spring.io/spring-boot/docs/3.3.4/reference/htmlsingle/index.html#web)
-* [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/3.3.4/reference/htmlsingle/index.html#using.devtools)
-* [Spring Data JPA](https://docs.spring.io/spring-boot/docs/3.3.4/reference/htmlsingle/index.html#data.sql.jpa-and-spring-data)
+## Enunciado
+Magneto quiere reclutar la mayor cantidad de mutantes para poder luchar contra los X-Mens.
 
-### Guides
-The following guides illustrate how to use some features concretely:
+Te ha contratado a ti para que desarrolles un proyecto que detecte si un humano es mutante basándose en su secuencia de ADN.
 
-* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
-* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
-* [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
-* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+Para eso te ha pedido crear un programa con un método o función con la siguiente firma:
 
-### Additional Links
-These additional references should also help you:h
+```java
+ boolean isMutant(String[] dna);
+```
 
-* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+En donde recibirás como parámetro un array de Strings que representan cada fila de una tabla de (NxN) con la secuencia del ADN. Las letras de los Strings solo pueden ser: (A,T,C,G), las cuales representa cada base nitrogenada del ADN.
 
+Sabrás si un humano es mutante, si encuentras **más de una secuencia de cuatro letras iguales**, de forma oblicua, horizontal o vertical.
+## Instrucciones de cómo ejecutar el programa o la API
+
+En este repositorio se encuentra el proyecto completo con lo requerido en el informe. A continuación se dará instrucciones de como ejecutar el proyecto en distintas situaciones:
+
+#### En local:
+1. Se debe ejecutar el archivo `MutantDetectorApplication` que funciona como el `main` de nuestra aplicación.
+2. A continuación una vez que en la consola muestre *funcionando* podremos interactuar con la aplicación localmente.
+3. Para poder entrar a la consola de H2 usamos `http://localhost:8080/h2-console/`
+4. Para entrar al Swagger usamos `http://localhost:8080/swagger-ui/index.html`
+5. En la consola estarán las tablas `DNA`, `DNA_AUD`, `REVISION_INFO` totalmente funcionales.
+6. En el Swagger se encontraran de manera graficas todos los servicios funcionales. Entre estos estará el requerido llamado mutant.
+7. Para poder realizar un post entraremos al servicio `/mutant` y seguiremos este cuerpo de ejemplo.
+
+```Json
+{
+    "dna": [
+      "ATGCGA",
+      "CAGTGC",
+      "TTATGT",
+      "AGAAGG",
+      "CCCCTA",
+      "TCACTG"
+    ]
+  }
+```
+8. Para poder realizarlo en Postman se sigue la misma lógica, se deberá crear una `request` por cada servicio, a continuación se darán las urls:
+```
+http://localhost:8080/mutant (POST)
+http://localhost:8080/find/1 (GET)
+http://localhost:8080/all (GET)
+http://localhost:8080/delete/1 (DELETE)
+http://localhost:8080/stats (GET)
+```
+> Borrar espacios que hay entre url y tipo de consulta.
+
+#### En Render
+En Render seguiremos la misma lógica pero usando la `url` de Render de nuestro deploy `https://mutant-detector-50113.onrender.com`, a continuación daré los pasos a seguir:
+1. Para acceder a Swagger usaremos la siguiente URL `https://mutant-detector-50113.onrender.com/swagger-ui/index.html` (esto puede tardar).
+2. Una vez cargado nuestro Swagger podremos realizar las consultas mediante los servicios. Para poder realizar un post entraremos al servicio `/mutant` y seguiremos este cuerpo de ejemplo.
+
+```Json
+{
+    "dna": [
+      "ATGCGA",
+      "CAGTGC",
+      "TTATGT",
+      "AGAAGG",
+      "CCCCTA",
+      "TCACTG"
+    ]
+  }
+```
+3. Para poder realizarlo en Postman se sigue la misma lógica, se deberá crear una `request` por cada servicio, a continuación se darán las urls:
+```
+https://mutant-detector-50113.onrender.com/mutant (POST)
+https://mutant-detector-50113.onrender.com/find/1 (GET)
+https://mutant-detector-50113.onrender.com/all (GET)
+https://mutant-detector-50113.onrender.com/delete/1 (DELETE)
+https://mutant-detector-50113.onrender.com/stats (GET)
+```
+> Borrar espacios que hay entre url y tipo de consulta.
+
+### Aspectos a saber:
+
+[También dentro del proyecto se encuentra el pdf para los documentos solicitados, este estará en la carpeta documents de este repositorio.](documentation/documentación.pdf) <- CLICK AQUI
--- src/main/java/com/magneto/mutantdetector/services/BaseServiceImpl.java
+++ src/main/java/com/magneto/mutantdetector/services/BaseServiceImpl.java
@@ -13,8 +13,10 @@ import java.util.stream.IntStream;
 
 public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> implements BaseService<E, ID> {
 
+    // Repositorio base para manejar operaciones de persistencia
     protected BaseRepository<E, ID> baseRepository;
 
+    // Constructor que recibe el repositorio base
     public BaseServiceImpl(BaseRepository<E, ID> baseRepository){
         this.baseRepository = baseRepository;
     }
@@ -22,10 +24,11 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Override
     @Transactional
     public List<E> findAll() throws Exception {
+        // Busca y devuelve todas las entidades
         try {
             List<E> entities = baseRepository.findAll();
             return entities;
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
@@ -33,10 +36,11 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Override
     @Transactional
     public E findBy(ID id) throws Exception {
+        // Busca y devuelve una entidad por su ID
         try {
             Optional<E> enityOptional = baseRepository.findById(id);
             return enityOptional.get();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
@@ -44,49 +48,56 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Override
     @Transactional
     public E save(E entity) throws Exception {
+        // Metodo para guardar una entidad en el repositorio
         try {
-            String[] dna = ((DNA) entity).getDna();
+            String[] dna = ((DNA) entity).getDna(); // Obtiene el arreglo de ADN
 
+            // Verifica si el ADN es mutante y establece el resultado
             ((DNA) entity).setEsMutant(comprobarDNA(dna));
 
-            // Guardar la entidad
+            // Guarda la entidad en el repositorio
             entity = baseRepository.save(entity);
 
-            return entity;
+            return entity; // Retorna la entidad guardada
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
 
-    public static boolean comprobarDNA(String[] dna){
-        final int LONGITUD_SECUENCIA = 4;
-        final int SECUENCIAS_NECESARIAS = 2;
-        DNAverification.validateDNA(dna);
+    public static boolean comprobarDNA(String[] dna) {
+        // Verifica si el ADN cumple con las características de un mutante
+        final int LONGITUD_SECUENCIA = 4; // Longitud mínima de la secuencia
+        final int SECUENCIAS_NECESARIAS = 2; // Secuencias necesarias para ser considerado mutante
+        DNAverification.validateDNA(dna); // Valida el ADN
 
+        // Cuenta cuántas secuencias mutantes se han encontrado
         long secuenciasEncontradas = IntStream.range(0, dna.length)
-                .boxed() // Convertimos a Stream<Integer>
+                .boxed() // Convierte a Stream<Integer>
                 .flatMap(fila -> IntStream.range(0, dna[0].length())
                         .mapToObj(columna -> new int[]{fila, columna}))
-                .filter(pos -> esParteDeSecuencia(dna, pos[0], pos[1]))
+                .filter(pos -> esParteDeSecuencia(dna, pos[0], pos[1])) // Filtra las posiciones que son parte de una secuencia
                 .count();
 
+        // Retorna true si se encontraron suficientes secuencias mutantes
         boolean esMutante = secuenciasEncontradas >= SECUENCIAS_NECESARIAS;
         return esMutante;
     }
 
     private static boolean esParteDeSecuencia(String[] dna, int fila, int columna) {
+        // Verifica si hay una secuencia mutante en la posición dada
         return buscarSecuencia(dna, fila, columna, 0, 1) || // Horizontal
                 buscarSecuencia(dna, fila, columna, 1, 0) || // Vertical
-                buscarSecuencia(dna, fila, columna, 1, 1) || // Diagonal izq -> der
-                buscarSecuencia(dna, fila, columna, 1, -1);  // Diagonal der -> izq
+                buscarSecuencia(dna, fila, columna, 1, 1) || // Diagonal izquierda a derecha
+                buscarSecuencia(dna, fila, columna, 1, -1);  // Diagonal derecha a izquierda
     }
 
-
     private static boolean buscarSecuencia(String[] dna, int fila, int columna, int incFila, int incColumna) {
+        // Busca una secuencia de ADN a partir de la posición inicial en la dirección especificada
         int filas = dna.length;
         int columnas = dna[0].length();
-        char caracterInicial = dna[fila].charAt(columna);
+        char caracterInicial = dna[fila].charAt(columna); // Caracter inicial para la búsqueda
 
+        // Verifica que los siguientes caracteres en la secuencia coincidan con el inicial
         return IntStream.range(1, 4) // LONGITUD_SECUENCIA = 4
                 .allMatch(k -> {
                     int nuevaFila = fila + k * incFila;
@@ -99,19 +110,18 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
                 });
     }
 
-
-
     @Override
     @Transactional
     public boolean delete(ID id) throws Exception {
+        // Elimina una entidad por su ID
         try {
-            if (baseRepository.existsById(id)){
+            if (baseRepository.existsById(id)) {
                 baseRepository.deleteById(id);
-                return true;
-            }else {
+                return true; // Retorna true si la eliminación fue exitosa
+            } else {
                 throw new Exception();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
